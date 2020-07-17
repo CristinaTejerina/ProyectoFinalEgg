f49df52841f52c3dde27b2e5d88c1a3bc713caa9
GimenezMartin <martin.gimenez.ghastin@gmail.com>
2020-07-17 20:55:39 -0300

    asd

--- tutores/src/main/java/com/web/tutores/Controladores/TutorControlador.java
+++ tutores/src/main/java/com/web/tutores/Controladores/TutorControlador.java
@@ -1,50 +1,10 @@
 package com.web.tutores.Controladores;
 
-import com.web.tutores.Entidades.Zona;
-import com.web.tutores.Repositorios.ZonaRepositorio;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/tutor")
 public class TutorControlador {
     
-     @Autowired
-    private ZonaRepositorio zonaRepositorio;
     
-       @GetMapping("/registro")
-    public String registro(ModelMap modelo) {
-        List<Zona> zonas = zonaRepositorio.findAll();
-        modelo.put("zonas", zonas);
-        return "registro.html";
-    }
-    @GetMapping("/modificar")
-    public String modificar(ModelMap modelo) {
-        List<Zona> zonas = zonaRepositorio.findAll();
-        modelo.put("zonas", zonas);
-        return "modificar.html";
-    }
-    
-    @GetMapping("/deshabilitar")
-    public String deshabilitar(ModelMap modelo) {
-        List<Zona> zonas = zonaRepositorio.findAll();
-        modelo.put("zonas", zonas);
-        return "deshabilitar.html";  
-    }
-    
-    @GetMapping("/habilitar")
-    public String habilitar(ModelMap modelo) {
-        List<Zona> zonas = zonaRepositorio.findAll();
-        modelo.put("zonas", zonas);
-        return "habilitar.html";
-    }
-    
-   
-        
-        
     
 }
